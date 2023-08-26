7a672825683f3023a0a696b77e4c5e478348a7ea
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-26 20:46:34 -0300

    - Início da Jornada Configurando seu VsCode Mapa do curso
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario

--- ReactJS-Primeiro-APP/src/App.jsx
+++ ReactJS-Primeiro-APP/src/App.jsx
@@ -1,21 +1,72 @@
 import { useState } from "react";
-import Nome from "./components/Nome";
 
 const App = () => {
-  const [aluno, setAluno] = useState("Sujeito Programador");
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [idade, setIdade] = useState("");
+  const [user, setUser] = useState([]);
 
-  function handleChangeName(nome) {
-    setAluno(nome);
+  function handleRegister(e) {
+    e.preventDefault();
+
+    alert("Usuário registrado com sucesso!");
+
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    });
   }
 
   return (
     <div>
-      <h1>Componente App</h1>
+      <h1>Cadastrando usuário</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Name:</label>
+        <br />
+        <input
+          placeholder='Digite seu nome'
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          type='text'
+        />
+        <br />
+
+        <label>Email:</label>
+        <br />
+        <input
+          placeholder='Digite seu email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+        />
+        <br />
+
+        <label>Idade:</label>
+        <br />
+        <input
+          placeholder='Digite sua idade'
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+          type='number'
+        />
+        <br />
+
+        <button type='submit'>Registrar</button>
+      </form>
+
       <br />
-      <h2>Olá: {aluno}</h2>
-      <button onClick={() => handleChangeName("Lucas Oliveira")}>
-        Mudar Nome
-      </button>
+      <br />
+
+      <div>
+        <span>Bem-vindo: {user.nome}</span>
+        <br />
+        <span>Idade: {user.idade}</span>
+        <br />
+        <span>Email: {user.email}</span>
+        <br />
+      </div>
     </div>
   );
 };
