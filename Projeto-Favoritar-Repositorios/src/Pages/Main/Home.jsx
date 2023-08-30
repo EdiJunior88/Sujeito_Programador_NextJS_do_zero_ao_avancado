5f02cbfdd956e687b70ae867989bfcebffba1936
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-29 23:16:11 -0300

    - Início da Jornada Configurando seu VsCode Mapa do curso
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos

--- Projeto-Favoritar-Repositorios/src/Pages/Main/Home.jsx
+++ Projeto-Favoritar-Repositorios/src/Pages/Main/Home.jsx
@@ -1,7 +1,36 @@
 import { Container, Form, SubmitButton } from "./styled";
 import { FaGithub, FaPlus } from "react-icons/fa";
+import { useCallback, useState } from "react";
+import API from "../../Services/API";
 
 const Home = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState([]);
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      async function submit() {
+        const response = await API.get(`/repos/${newRepo}`);
+
+        const data = {
+          name: response.data.full_name,
+        };
+
+        setRepositorios([...repositorios, data]);
+        setNewRepo("");
+      }
+
+      submit();
+    },
+    [newRepo, repositorios]
+  );
+
+  function handleInputChange(event) {
+    setNewRepo(event.target.value);
+  }
+
   return (
     <Container>
       <h1>
@@ -9,10 +38,15 @@ const Home = () => {
         Meus Repositórios
       </h1>
 
-      <Form onSubmit={() => {}}>
-        <input type='text' placeholder='Adicionar Repositório' />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Adicionar Repositório'
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
-        <SubmitButton type='submit'>
+        <SubmitButton>
           <FaPlus color='#FFF' size={14} />
         </SubmitButton>
       </Form>
