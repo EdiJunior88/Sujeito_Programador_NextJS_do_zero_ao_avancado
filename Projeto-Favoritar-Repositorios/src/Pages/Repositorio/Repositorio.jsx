247d1acf009835f973610af7ab33cba544592384
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-30 22:42:00 -0300

    - Início da Jornada Configurando seu VsCode Mapa do curso
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos
    Buscando repos pt2
    Listando repositórios
    Salvando repositórios
    Pagina detalhes
    Carregando detalhes

--- Projeto-Favoritar-Repositorios/src/Pages/Repositorio/Repositorio.jsx
+++ Projeto-Favoritar-Repositorios/src/Pages/Repositorio/Repositorio.jsx
@@ -1,9 +1,38 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import { Container } from "./styled";
+import { useEffect } from "react";
+import API from "../../Services/API";
+import { useState } from "react";
 
-const Repositorio = ({ match }) => {
+const Repositorio = () => {
   const name = useParams();
+  const [repositorio, setRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return <h1>{decodeURIComponent(name.repositorio)}</h1>;
+  useEffect(() => {
+    async function load() {
+      const nomeRepo = decodeURIComponent(name.repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        API.get(`/repos/${nomeRepo}`),
+        API.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [name.repositorio]);
+
+  return <Container></Container>;
 };
 
 export default Repositorio;
