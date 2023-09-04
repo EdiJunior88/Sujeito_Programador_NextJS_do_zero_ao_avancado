19ef55e8e9435abb3a3b501e692a6cda7b69ae4a
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-09-04 20:05:06 -0300

    - Início da Jornada Configurando seu VsCode Mapa do curso
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos
    Buscando repos pt2
    Listando repositórios
    Salvando repositórios
    Pagina detalhes
    Carregando detalhes
    Exibindo detalhes
    Exibindo issues
    Criando paginação
    Criando filtros
    
    - Entendo NextJS
    O que é NextJS?
    Entendendo a estrutura
    Comandos para criar seu primeiro projeto
    Conhecendo o projeto
    Criando projeto

--- projeto-board-tarefas/src/app/page.tsx
+++ projeto-board-tarefas/src/app/page.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import styles from "../../styles/page.module.css";
+import Image from "next/image";
+import heroImg from "../../public/assets/hero.png";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt='logo Tarefas+'
+            src={heroImg}
+            priority
+          />
+        </div>
+
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e tarefas
+        </h1>
+      </main>
+    </div>
+  );
 }
