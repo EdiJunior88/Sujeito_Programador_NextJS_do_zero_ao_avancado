9fdc257861dd86bc17a63dfff23bc0dc3ae5ad33
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-12 00:31:06 -0300

    - Início da Jornada Configurando seu VsCode Mapa do curso
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos
    Buscando repos pt2
    Listando repositórios
    Salvando repositórios
    Pagina detalhes
    Carregando detalhes
    Exibindo detalhes
    Exibindo issues
    Criando paginação
    Criando filtros
    
    - Entendo NextJS
    O que é NextJS?
    Entendendo a estrutura
    Comandos para criar seu primeiro projeto
    Conhecendo o projeto
    Criando projeto
    Criando header
    Configurando sistema de login
    Criando login
    Criando rota de admin
    - Início da Jornada
    Configurando seu VsCode
    Mapa do curso
    
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos
    Buscando repos pt2
    Listando repositórios
    Salvando repositórios
    Pagina detalhes
    Carregando detalhes
    Exibindo detalhes
    Exibindo issues
    Criando paginação
    Criando filtros
    
    - Entendo NextJS
    O que é NextJS?
    Entendendo a estrutura
    Comandos para criar seu primeiro projeto
    Conhecendo o projeto
    Criando projeto
    Criando header
    Configurando sistema de login
    Criando login
    Criando rota de admin
    Criando dashboard
    Criando dashboard pt2
    Cadastrar tarefas

--- projeto-board-tarefas/src/app/components/textarea/page.tsx
+++ projeto-board-tarefas/src/app/components/textarea/page.tsx
@@ -1,7 +1,19 @@
 import styles from "./textarea.module.css";
 
-const Textarea = ({ ...rest }) => {
-  return <textarea className={styles.textarea} {...rest}></textarea>;
+interface TextareaProps {
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const Textarea = ({ placeholder, value, onChange }: TextareaProps) => {
+  return (
+    <textarea
+      className={styles.textarea}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}></textarea>
+  );
 };
 
 export default Textarea;
