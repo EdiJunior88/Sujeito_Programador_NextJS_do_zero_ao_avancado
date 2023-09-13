066f911cebf0d2ffd505b834e1fd80b644f6a6c1
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-12 22:46:10 -0300

    - Início da Jornada Configurando seu VsCode Mapa do curso
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos
    Buscando repos pt2
    Listando repositórios
    Salvando repositórios
    Pagina detalhes
    Carregando detalhes
    Exibindo detalhes
    Exibindo issues
    Criando paginação
    Criando filtros
    
    - Entendo NextJS
    O que é NextJS?
    Entendendo a estrutura
    Comandos para criar seu primeiro projeto
    Conhecendo o projeto
    Criando projeto
    Criando header
    Configurando sistema de login
    Criando login
    Criando rota de admin
    - Início da Jornada
    Configurando seu VsCode
    Mapa do curso
    
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos
    Buscando repos pt2
    Listando repositórios
    Salvando repositórios
    Pagina detalhes
    Carregando detalhes
    Exibindo detalhes
    Exibindo issues
    Criando paginação
    Criando filtros
    
    - Entendo NextJS
    O que é NextJS?
    Entendendo a estrutura
    Comandos para criar seu primeiro projeto
    Conhecendo o projeto
    Criando projeto
    Criando header
    Configurando sistema de login
    Criando login
    Criando rota de admin
    Criando dashboard
    Criando dashboard pt2
    Cadastrar tarefas
    Cadastrando no banco de dados

--- projeto-board-tarefas/src/lib/session.ts
+++ projeto-board-tarefas/src/lib/session.ts
@@ -1,3 +1,19 @@
+// import { getServerSession } from "next-auth/next";
+
+// import { authOptions } from "../app/api/auth/[...nextauth]/route";
+
+// export async function getCurrentUser() {
+//   const session = await getServerSession(authOptions);
+
+//   if (session?.user?.email) {
+//     return {
+//       user: {
+//         email: session.user.email,
+//       },
+//     };
+//   }
+// }
+
 import { getServerSession } from "next-auth/next";
 
 import { authOptions } from "../app/api/auth/[...nextauth]/route";
@@ -5,11 +21,9 @@ import { authOptions } from "../app/api/auth/[...nextauth]/route";
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
 
-  if (session?.user?.email) {
-    return {
-      user: {
-        email: session.user.email,
-      },
-    };
-  }
+  return {
+    user: {
+      email: session?.user?.email,
+    },
+  };
 }
