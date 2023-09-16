dc3801611ac079cb033d96368cfbf0a4f6dcbc38
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-15 21:16:27 -0300

    - Início da Jornada Configurando seu VsCode Mapa do curso
    - Começando com ReactJS
    Primeiros passos
    Primeiro projeto
    Propriedades e componente
    Trabalhando com useState
    Manipulando formulario
    Praticando
    useEffect
    Requisições HTTP
    Trabalhando com Rotas
    Trabalhando com Rotas pt2
    Aplicando Styled Components
    
    - Primeiro Projeto
    Conhecendo projeto
    Começando projeto
    Criando estilos
    Estilizando pagina Main
    Buscando repos
    Buscando repos pt2
    Listando repositórios
    Salvando repositórios
    Pagina detalhes
    Carregando detalhes
    Exibindo detalhes
    Exibindo issues
    Criando paginação
    Criando filtros
    
    - Entendendo NextJS
    Entendendo a estrutura
    Comandos para criar seu primeiro projeto
    
    - Projeto Board Tarefas
    Criando projeto
    Criando header
    Configurando sistema de login
    Criando login
    Criando rota de admin
    Criando dashboard
    Criando dashboard pt2
    Cadastrar tarefas
    Cadastrando no banco de dados
    Listando itens

--- projeto-board-tarefas/src/app/dashboard-client/page.tsx
+++ projeto-board-tarefas/src/app/dashboard-client/page.tsx
@@ -3,17 +3,63 @@ import styles from "./dashboard-client.module.css";
 import TextArea from "../components/textarea/TextAreaComponent";
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { db } from "../../services/firebaseConnection";
-import { addDoc, collection } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 
 interface InterfaceDashboardClient {
   user: { email: string } | undefined;
 }
 
+interface InterfaceTasksProps {
+  id: string;
+  created: Date;
+  public: boolean;
+  tarefa: string;
+  user: string;
+}
+
 const DashboardClient = ({ user }: InterfaceDashboardClient) => {
   const [input, setInput] = useState("");
   const [publicTask, setPublicTask] = useState(false);
+  const [tasks, setTasks] = useState<InterfaceTasksProps[]>([]);
+
+  useEffect(() => {
+    async function loadTarefas() {
+      const tarefasRef = collection(db, "tarefas");
+      const filter = query(
+        tarefasRef,
+        orderBy("created", "desc"),
+        where("user", "==", { user })
+      );
+
+      onSnapshot(filter, (snapshot) => {
+        console.log(snapshot);
+        let lista = [] as InterfaceTasksProps[];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            created: doc.data().created,
+            public: doc.data().public,
+            tarefa: doc.data().tarefa,
+            user: doc.data().user.user,
+          });
+        });
+
+        setTasks(lista);
+      });
+    }
+
+    loadTarefas();
+  }, [user]);
 
   const handleChangePublic = (event: ChangeEvent<HTMLInputElement>) => {
     setPublicTask(event.target.checked);
@@ -77,21 +123,25 @@ const DashboardClient = ({ user }: InterfaceDashboardClient) => {
       <section className={styles.taskContainer}>
         <h1>Minhas Tarefas</h1>
 
-        <article className={styles.task}>
-          <div className={styles.tagContainer}>
-            <label className={styles.tag}>PÚBLICO</label>
-            <button className={styles.shareButton}>
-              <FiShare2 size={22} color='#3183ff' />
-            </button>
-          </div>
+        {tasks.map((item) => (
+          <article key={item.id} className={styles.task}>
+            {item.public && (
+              <div className={styles.tagContainer}>
+                <label className={styles.tag}>PÚBLICO</label>
+                <button className={styles.shareButton}>
+                  <FiShare2 size={22} color='#3183ff' />
+                </button>
+              </div>
+            )}
 
-          <div className={styles.taskContent}>
-            <p>Minha primeira tarefa de exemplo!</p>
-            <button className={styles.trashButton}>
-              <FaTrash size={24} color='#ea3140' />
-            </button>
-          </div>
-        </article>
+            <div className={styles.taskContent}>
+              <p>{item.tarefa}</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color='#ea3140' />
+              </button>
+            </div>
+          </article>
+        ))}
       </section>
     </main>
   );
